2b89333a3068ae7c1ef64d63d7d09cff1d2c137a
ottjw <ottjw@yahoo.com>
2016-03-08 19:35:15 +0000

    initial commit

---
+++ db/migrate/20160308192518_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :name, null: false
+      t.boolean :completed, default: false
+      t.date :goal_date, 
+      t.belongs_to :list
+
+      t.timestamps null: false
+    end
+  end
+end
