f530295bde77e5cbd997cbca6a55232ba333f441
jsquire4 <jacobmsquire@yahoo.com>
2016-03-08 15:11:38 -0700

    list controller updated

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,48 @@
 class ListsController < ApplicationController
   def index
-  end
-
-  def new
+    @lists = current_user.lists.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
   end
+
+  def update
+    if @list.update(list_params)
+      redirect_to user_lists_path(current_user, @list)
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = current_user.list.new(list_params)
+    if @list.save
+      redirect_to user_lists_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to root
+  end
+
+  private
+    def list_params
+      params.require(:list).permit(:name, :date)
+    end
+
+
+    def list
+      @list = current_user.lists.find(params[:id])
+    end
 end
